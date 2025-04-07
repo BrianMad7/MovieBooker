0a6991a155a367c3a232dc41c12e35d04bb7fb92
BrianMad7 <134330198+BrianMad7@users.noreply.github.com>
2025-04-07 10:58:24 +0200

    Add token generation function

---
+++ auth.js
@@ -0,0 +1,16 @@
+function generateToken(user) {
+    const crypt = btoa(JSON.stringify(user));
+    return crypt;
+}
+
+function verifyToken(token) {
+    const decode = atob(token);
+    // https://www.w3schools.com/jsref/met_win_atob.asp
+    return decode;
+}
+
+
+const test = {id: 1, name: "Brian", password: "Azerty", mail: "Brian@nest.fr"};
+const token = generateToken(test);
+console.log("Token:", token);
+console.log("decode:", verifyToken(token))
